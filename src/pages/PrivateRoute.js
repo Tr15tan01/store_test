df768a3adf68ca9820e0556f8502d01d798bc4ae
tr15tan01 <t.varamashvili@gmail.com>
2023-10-20 22:57:35 +0400

    n 2

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,11 +1,14 @@
 import { Navigate, useLocation } from "react-router-dom";
 import { useUserContext } from "../context/user_context";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function PriveteRoute({ children }) {
-  const { myUser } = useUserContext();
+  // const { myUser } = useUserContext();
+  const { user } = useAuth0();
+
   const location = useLocation();
 
-  if (!myUser) {
+  if (!user) {
     return <Navigate to="/" state={{ from: location }} />;
   }
 
